d34944dab448cdf4f117f6e4e961bd3e27b40c8d
Minjae Seon (Darkhost) <ddol0225@gmail.com>
2019-05-14 11:16:58 +0900

    Divide JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+﻿// Navbar System
+var divlist = ['main_map', 'food', 'study','rest', 'shuttle'];
+
+function mainnav_show(divname) {
+    for(var i = 0; i < divlist.length; i++) {
+        if(divname == divlist[i]) {
+            document.getElementById("mainnav_sbox").selectedIndex = i;
+            document.getElementById(divname).style.display = "";
+        }
+        else {
+            document.getElementById(divlist[i]).style.display = "none";
+        }
+    }
+}
+
+function openPopup(url) {
+    window.open(url, "WhereRU_popup", "width=500, height=800, toolbar=no, menubar=no, scrollbars=yes");
+}
---
+++ js/map.js
@@ -0,0 +1,14 @@
+﻿// e value : Select Item's ID (ex. gachonhall, collegeofit...)
+function mapselects(e) {
+    switch(e) {
+        case 'gachonhall': 
+            alert("가천관임ㅋ");
+            break;
+        case 'collegeofit':
+            alert("IT대학");
+            break; 
+        default:
+            alert(e);
+            break;
+    }
+}
