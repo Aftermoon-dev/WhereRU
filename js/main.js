deef249ad254ddad0baa7f6e2d2aa9b29d017f3a
Minjae Seon (Darkhost) <ddol0225@gmail.com>
2019-05-21 11:11:32 +0900

    Re-design

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-﻿// Navbar System
+﻿/* Side Menu Script */
+
+// Div List Array
 var divlist = ['main_map', 'food', 'study','rest', 'shuttle'];
 
-function mainnav_show(divname) {
+function mainside_show(divname) {
     for(var i = 0; i < divlist.length; i++) {
         if(divname == divlist[i]) {
-            document.getElementById("mainnav_sbox").selectedIndex = i;
             document.getElementById(divname).style.display = "";
         }
         else {
@@ -13,6 +14,40 @@ function mainnav_show(divname) {
     }
 }
 
+// Side Menu Open Check Variable
+var open = false;
+
+// Side Menu Function
+function openMenu() {
+    // Side Menu Div
+    var sidemenu = document.getElementById('sideMenu');
+
+    // Check Screen Size
+    var mq = window.matchMedia('screen and (max-width:950px)');
+
+    // If, not open sideMenu
+    if(!open) {
+        // If, not max-width:950px (PC)
+        if(mq.matches == false) {
+            // Set sideMenu Width
+            sidemenu.style.width = "250px";
+        }
+        // If not (Mobile)
+        else {
+            // Set sideMenu Width
+            sidemenu.style.width = "100px";
+        }
+        open = true;
+    }
+    // If, open sideMenu
+    else {
+        // Restore All Setting
+        sidemenu.style.width = "0px";
+        open = false;
+    }
+}
+
+// Popup
 function openPopup(url) {
     window.open(url, "WhereRU_popup", "width=500, height=800, toolbar=no, menubar=no, scrollbars=yes");
 }
