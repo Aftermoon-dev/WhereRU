a8dce04d99a32bb407470729b61e47e4c572e248
Minjae Seon (Darkhost) <ddol0225@gmail.com>
2019-05-26 21:59:40 +0900

    Add Content

--- js/main.js
+++ js/main.js
@@ -69,13 +69,20 @@ function showMessage(title, message) {
 function mapselects(e) {
     switch(e) {
         case 'gachonhall': 
-            showMessage("가천관", "가천관이에요");
+            showMessage("가천관", 
+                "<img src='./img/school/gachonhall.jpg' width='400' height='300'><br>대학 본부가 위치해있는 건물이며, 학생 쉼터 '라곰'이 1층에 있다.");
             break;
         case 'collegeofit':
-            showMessage("IT융합대학", "IT융합대학");
+            showMessage("IT융합대학", 
+                "<img src='./img/school/it.jpg' width='400' height='300'><br>구) 새롬관<br>IT융합대학 학생들이 이용한다.");
             break; 
         case 'visiontower':
-                showMessage("비전타워", "비전타워");
+            showMessage("비전타워", 
+                "<img src='./img/school/visiontower.jpg' width='400' height='300'><br>A동과 B동으로 나누어져 있으며, 분당선 가천대역과 연결되어 있는 건물이다.");
+            break;
+        case 'graduateschooledu':
+            showMessage("교육대학원", 
+                "<img src='./img/school/edu.jpg' width='400' height='300'><br>구) 아름관<br>사회과학대학, 예술대학 학생들이 이용하며, 지하에는 식당과 매점이 있다.");
             break;
         default:
             showMessage(e, "id는 " + e);
